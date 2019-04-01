dbdf34f1c42e3b07db0b5e9dbb071238ec7cebff
JorisVanEijden <joris.vaneijden@gmail.com>
2019-04-01 12:30:33 +0200

    Add laserbeams

---
+++ Assets/Scripts/LaserSequence.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class LaserSequence : MonoBehaviour
+{
+    [SerializeField] private GameObject[] laserBeams = null;
+    [SerializeField] private float intervalSeconds = 3f;
+    [SerializeField] private int modulo = 2;
+
+    private float _currentTime;
+    private int _currentIndex;
+
+    private void Start()
+    {
+        // enable and disable the appropriate beams
+        SwitchBeams();
+    }
+
+    private void SwitchBeams()
+    {
+        for (int i = 0; i < laserBeams.Length; i++)
+        {
+            laserBeams[i].SetActive((i + _currentIndex) % modulo == 0);
+        }
+    }
+
+    private void Update()
+    {
+        _currentTime += Time.deltaTime;
+        if (_currentTime >= intervalSeconds)
+        {
+            // on interval go to next set
+            _currentTime = 0f;
+            _currentIndex = (_currentIndex + 1) % laserBeams.Length;
+            SwitchBeams();
+        }
+    }
+}
