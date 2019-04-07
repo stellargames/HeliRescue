b953548bbc3155bfebbe60c594774835e1c54732
JorisVanEijden <joris.vaneijden@gmail.com>
2019-04-07 19:58:18 +0200

    made chasingEnemy work

--- Assets/Scripts/ChasingEnemy.cs
+++ Assets/Scripts/ChasingEnemy.cs
@@ -1,40 +1,99 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.Assertions;
 
+[RequireComponent(typeof(CircleCollider2D))]
 public class ChasingEnemy : MonoBehaviour
 {
+    [SerializeField] private float chaseSpeed = 0.05f;
+    [SerializeField] private float fadeSpeed = 0.5f;
+    [SerializeField] private Transform body = null;
+
     private bool _activated;
-    [SerializeField] private float fadeTime = 2f;
+    private Transform _currentTarget;
+    private Coroutine _currentFadeRoutine;
 
     private void Awake()
     {
         SetChildrenActive(false);
+        Assert.IsNotNull(body,
+            $"Please assign a body to the ChasingEnemy {gameObject.name}");
+        body.localScale = Vector3.zero;
     }
 
-    private void OnTriggerEnter2D(Collider2D collider)
+    private void Update()
     {
-        if (!_activated && collider.CompareTag("Player"))
+        if (!_activated) return;
+
+        if (_currentTarget == null)
         {
+            StartCoroutine(Deactivate());
+        }
+        else
+        {
+            transform.position =
+                Vector3.MoveTowards(transform.position, _currentTarget.position,
+                    chaseSpeed);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!_activated && other.CompareTag("Player"))
+        {
+            _currentTarget = other.transform;
             Activate();
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (_activated)
+        {
+            StartCoroutine(Die());
+        }
+    }
+
+    private IEnumerator Die()
+    {
+        yield return Deactivate();
+        Destroy(gameObject);
     }
 
     private void Activate()
     {
         _activated = true;
         SetChildrenActive(true);
-        StartCoroutine(FadeIn());
+        _currentFadeRoutine = StartCoroutine(FadeBodyIn());
     }
 
-    private IEnumerator FadeIn()
+    private IEnumerator Deactivate()
     {
-        transform.localScale = Vector3.zero;
-        var elapsedTime = 0f;
-        while (elapsedTime < fadeTime)
+        _activated = false;
+        if (_currentFadeRoutine != null) StopCoroutine(_currentFadeRoutine);
+        yield return FadeBodyOut();
+        SetChildrenActive(false);
+    }
+
+    private IEnumerator FadeBodyIn()
+    {
+        var currentScale = body.transform.localScale;
+        while (currentScale.sqrMagnitude < 3f)
         {
+            currentScale += Time.deltaTime * fadeSpeed * Vector3.one;
+            body.transform.localScale = currentScale;
             yield return null;
-            transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, elapsedTime / fadeTime);
-            elapsedTime += Time.deltaTime;
+        }
+    }
+
+    private IEnumerator FadeBodyOut()
+    {
+        var currentScale = body.transform.localScale;
+        while (currentScale.sqrMagnitude > 0.1f)
+        {
+            currentScale -= Time.deltaTime * fadeSpeed * Vector3.one;
+            body.transform.localScale = currentScale;
+            yield return null;
         }
     }
 
@@ -45,5 +104,4 @@ public class ChasingEnemy : MonoBehaviour
             transform.GetChild(i).gameObject.SetActive(value);
         }
     }
-
 }
