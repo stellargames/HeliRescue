a6dde03c3a538fcbc58dfa15048a3bfe2ffb4624
JorisVanEijden <joris.vaneijden@gmail.com>
2019-05-22 19:47:45 +0200

    lightning enemy continued

---
+++ Assets/Scripts/WaypointFollower.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class WaypointFollower : MonoBehaviour
+{
+    [SerializeField] private Transform[] waypoints;
+
+#if UNITY_EDITOR
+    private void Update()
+    {
+        var position = transform.position;
+        foreach (var waypoint in waypoints)
+        {
+            Debug.DrawLine(position, waypoint.position, Color.green);
+            position = waypoint.position;
+        }
+    }
+#endif
+}
